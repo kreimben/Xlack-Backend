1abbd304e42e4599c49a2c330ff02bfd948514c8
Aksidion Kreimben <aksidion@kreimben.com>
2022-07-04 18:47:28 +0900

    Implement connect to db server.

--- app/ready.py
+++ app/ready.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import app.model.database as database
+
+
+def ready_for_db():
+    database.Base.metadata.create_all(bind=database.engine)
+    print(f'Ready for db!')
 
 
 def ready_app() -> FastAPI:
+    ready_for_db()
+
     app = FastAPI(
         title='Xlack',
         description='Furthermore Workspace.',
