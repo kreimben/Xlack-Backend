8a0764585d7afce2e2e4ea039b8b2465550d2f26
Aksidion Kreimben <aksidion@kreimben.com>
2022-07-05 15:10:35 +0900

    Fix redirect_uri algorithms.

--- app/utils/github_auth.py
+++ app/utils/github_auth.py
@@ -3,12 +3,16 @@ import requests
 
 
 def exchange_code_for_access_token(code: str, redirect_uri: str | None = None):
-    res = requests.post('https://github.com/login/oauth/access_token', params={
+    params = {
         'client_id': os.getenv('GITHUB_CLIENT_ID'),
         'client_secret': os.getenv('GITHUB_CLIENT_SECRET'),
-        'code': code,
-        'redirect_uri': redirect_uri
-    })
+        'code': code
+    }
+
+    if redirect_uri is not None:
+        params.update({'redirect_uri': redirect_uri})
+
+    res = requests.post('https://github.com/login/oauth/access_token', params=params)
 
     return res.json()
 
