aed6ea8568b2bccbc1468d1eb4fda803082be3bc
Aksidion Kreimben <aksidion@kreimben.com>
2022-07-05 16:08:53 +0900

    Fix algo.

--- app/auth/router.py
+++ app/auth/router.py
@@ -9,17 +9,16 @@ router = APIRouter(prefix='/auth', tags=['auth'])
 
 @router.get('/login')
 async def login_github():
-
     client_id = os.getenv('GITHUB_CLIENT_ID')
-    redirect_uri = 'https://xlack.kreimben.com/auth/redirect/github'
+    # redirect_uri = 'https://xlack.kreimben.com/auth/redirect/github'
     scope = 'read:user'
-    url = f'https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}'
+    url = f'https://github.com/login/oauth/authorize?client_id={client_id}&scope={scope}'
     print(f'url: {url}')
     return RedirectResponse(url)
 
 
 @router.get('/redirect/github')
-async def redirect_github(request: Request):
+async def redirect_github(request: Request, code: str):
     """
     This function deal with after redirect from client.
 
@@ -27,8 +26,8 @@ async def redirect_github(request: Request):
     """
 
     print(f'params: {request.query_params}')
-
-    res = exchange_code_for_access_token('sdf')
+    print(f'code: {code}')
+    res = exchange_code_for_access_token(code)
 
     print(f'res: {res}')
 
