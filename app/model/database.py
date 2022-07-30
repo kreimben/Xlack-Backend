32ff2c606af66b3d9f67a61f5a8fd0f699e4de7c
Aksidion Kreimben <aksidion@kreimben.com>
2022-07-31 03:00:08 +0900

    Change string value.

--- app/model/database.py
+++ app/model/database.py
@@ -2,7 +2,6 @@ import os
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.engine import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -14,12 +13,13 @@ __pw = os.getenv('MARIADB_USER_PASSWORD')
 __host = os.getenv('MARIADB_HOST')
 __port = os.getenv('MARIADB_PORT')
 __db = os.getenv('MARIADB_DB')
-SQLALCHEMY_DATABASE_URL = URL('mariadb+pymysql',
-                              username=__id,
-                              password=__pw,
-                              host=__host,
-                              port=__port,
-                              database=__db)  # f"mysql+pymysql://{__id}:{__pw}@{__host}/{__db}"
+# SQLALCHEMY_DATABASE_URL = URL('mariadb+pymysql',
+#                               username=__id,
+#                               password=__pw,
+#                               host=__host,
+#                               port=__port,
+#                               database=__db)
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{__id}:{__pw}@{__host}:{__port}/{__db}?charset=utf8"
 
 print(f'SQLALCHEMY_DATABASE_URL: {SQLALCHEMY_DATABASE_URL}')
 
