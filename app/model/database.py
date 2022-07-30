cec236622651f18b8d314ac6a8214a0c9a597e73
Aksidion Kreimben <aksidion@kreimben.com>
2022-07-31 02:41:02 +0900

    Change the way approach database.

--- app/model/database.py
+++ app/model/database.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -11,8 +12,14 @@ load_dotenv()
 __id = os.getenv('MARIADB_USER_ID')
 __pw = os.getenv('MARIADB_USER_PASSWORD')
 __host = os.getenv('MARIADB_HOST')
+__port = os.getenv('MARIADB_PORT')
 __db = os.getenv('MARIADB_DB')
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{__id}:{__pw}@{__host}/{__db}"
+SQLALCHEMY_DATABASE_URL = URL('mysql+pymysql',
+                              username=__id,
+                              password=__pw,
+                              host=__host,
+                              port=__port,
+                              database=__db)  # f"mysql+pymysql://{__id}:{__pw}@{__host}/{__db}"
 
 print(f'SQLALCHEMY_DATABASE_URL: {SQLALCHEMY_DATABASE_URL}')
 
