274f36c43475bc7ad4ba4b63580a54eb2d809cd6
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-07 01:37:30 +0900

    Refactoring.

--- chat_channel/models.py
+++ chat_channel/models.py
@@ -4,9 +4,13 @@ from django.db import models
 # The reason I named `ChatChannel` is avoiding confusion with `django channels`.
 class ChatChannel(models.Model):
     # Should we make chatted user field?
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Channel'
+        verbose_name_plural = 'Channels'
 
     def __str__(self):
         return self.name
