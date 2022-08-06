e541f0fe8c59b20e795f354a45c53853e720157b
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-07 01:38:40 +0900

    Implement chat model.

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from chat_channel.models import ChatChannel
+
+
+class Chat(models.Model):
+    content = models.TextField(null=False, blank=False)
+    chatter_id = models.ForeignKey(User, on_delete=models.PROTECT, default=1, blank=False)
+    channel_id = models.ForeignKey(ChatChannel, on_delete=models.CASCADE, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Chat'
+        verbose_name_plural = 'Chats'
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'{self.channel_id} : {self.content}'
