2f64057dc80075ad5f3395ba3adc140ca5373358
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-07 01:38:31 +0900

    Implement consumer.

---
+++ chat/consumers.py
@@ -0,0 +1,22 @@
+import ujson
+
+from channels.generic.websocket import WebsocketConsumer
+
+
+class ChatConsumer(WebsocketConsumer):
+
+    async def connect(self):
+        self.accept()
+
+    async def receive(self, text_data=None, bytes_data=None):
+        text_data_json = ujson.loads(text_data)
+        message = text_data_json['message']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            ujson.dumps({
+                'message': message
+            }))
+
+    # async def disconnect(self, code):
+    #     pass
