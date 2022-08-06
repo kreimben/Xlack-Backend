ae02f459e7057a4c50eb61d6546efba07811f3cd
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-06 18:01:40 +0900

    Add new path.

--- xlack/urls.py
+++ xlack/urls.py
@@ -35,7 +35,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path(os.getenv('DJANGO_REAL_ADMIN_URI'), admin.site.urls),
-    path('', include('rest_framework.urls', namespace='rest_framework')),
+    path('accounts/', include('rest_framework.urls', namespace='rest_framework')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger_documentation'),
 
     path('channel/', include('chat_channel.urls')),
