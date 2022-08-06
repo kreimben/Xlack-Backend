a0a46b6e66c14f4766938e8d1492e4bf0d5cccda
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-07 01:41:12 +0900

    Implement new asgi settings for channels (websockets for chatting).

--- xlack/asgi.py
+++ xlack/asgi.py
@@ -7,10 +7,33 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
+# import os
+#
+# from django.core.asgi import get_asgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xlack.settings')
+#
+# application = get_asgi_application()
+
+
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xlack.settings')
+import chat.routing
 
-application = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        )
+    )
+})
