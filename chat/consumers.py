56533c55dba9cb9fa0d0a6378fd4df66ffd7c844
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-08 01:30:04 +0900

    Add group given informations.

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,7 +9,10 @@ class ChatConsumer(JsonWebsocketConsumer):
         print(f'channel_id: {self.channel_id}')
         self.room_group_name = f'chat_{self.channel_id}'
 
-    async def connect(self):
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
         self.accept()
 
     async def receive(self, text_data=None, bytes_data=None):
