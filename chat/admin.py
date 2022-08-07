c6518ecfcef4e78947a54e3330e8f3b83db41646
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-08 02:41:36 +0900

    Refactoring.

--- chat/admin.py
+++ chat/admin.py
@@ -5,5 +5,5 @@ from chat.models import Chat
 
 @admin.register(Chat)
 class ChatAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Chat._meta.get_fields()]
-    search_fields = ['name']
+    list_display = ['id', 'message', 'chatter', 'channel', 'created_at']
+    search_fields = ['message', 'channel', 'chatter']
