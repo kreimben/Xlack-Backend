2d55d497f3d8fc18f44e5e5426ad0b9d618d784b
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-08 01:31:36 +0900

    Refactoring.

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,6 +1,7 @@
 import ujson
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import JsonWebsocketConsumer
 
-from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(JsonWebsocketConsumer):
 
