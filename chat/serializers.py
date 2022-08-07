d99cd683303c4e2c63462865fc9b6ba2d5fcce54
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-08 02:43:32 +0900

    Fix wrong things.

--- chat/serializers.py
+++ chat/serializers.py
@@ -5,7 +5,7 @@ from chat.models import Chat
 
 class ChatSerializer(serializers.ModelSerializer):
     id = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
-    chatter_id = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    chatter = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
 
     class Meta:
         model = Chat
