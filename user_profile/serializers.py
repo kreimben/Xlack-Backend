24f7088c9781ccd3456114029d701c5b8fc07fbd
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-13 00:26:08 +0900

    Refactoring according to warnings.

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -4,6 +4,8 @@ from user_profile.models import UserProfile
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+
     class Meta:
         model = UserProfile
         fields = '__all__'
