258722490a9b488bf8719d3e613e93583451582a
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-13 00:41:38 +0900

    Delete fucking user_custom.

--- user_custom/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from user_custom import views
-
-urlpatterns = [
-    path('github/login/', views.github_login, name='github_login'),
-    path('github/callback/', views.github_callback, name='github_callback'),
-    path('github/login/finish/', views.GithubLogin.as_view(), name='github_login_todjango'),
-]
