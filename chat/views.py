c254c0240aea75322d8838217c5d293f269266a0
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-20 05:49:07 +0900

    Add comments to swagger documentation.

--- chat/views.py
+++ chat/views.py
@@ -6,6 +6,10 @@ from chat.serializers import ChatSerializer
 
 
 class ChatView(generics.ListAPIView):
+    """
+    count는 전체 레코드의 수
+    next, previous는 이전, 이후의 url
+    """
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ChatSerializer
     pagination_class = LimitOffsetPagination
