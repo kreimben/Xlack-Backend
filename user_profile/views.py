71980a943d8a35d1909018cc08cd67f95ff7166d
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-20 10:02:21 +0900

    Add new logic for UserProfile.

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,4 +1,6 @@
+from allauth.socialaccount.models import SocialAccount
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 
 from user_profile.models import UserProfile
 from user_profile.serializers import UserProfileSerializer
@@ -10,3 +12,30 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
     permission_classes = [permissions.IsAuthenticated]
     lookup_field = 'user_id'
+
+    def list(self, request, *args, **kwargs):
+        social_users = SocialAccount.objects.all()
+
+        for user in social_users:
+            UserProfile.objects.get_or_create(
+                user_id=user.user_id,
+                github_id=user.extra_data['id'],
+                bio=user.extra_data['bio'],
+                thumbnail_url=user.extra_data['avatar_url']
+            )
+
+        s = [UserProfileSerializer(user) for user in self.get_queryset()]
+
+        return Response([user.data for user in s])
+
+    def retrieve(self, request, *args, **kwargs):
+        user_id = self.kwargs.get('user_id')
+        social_user = SocialAccount.objects.get(user_id=user_id)
+        user, _ = UserProfile.objects.get_or_create(
+            user_id=social_user.user_id,
+            github_id=social_user.extra_data['id'],
+            bio=social_user.extra_data['bio'],
+            thumbnail_url=social_user.extra_data['avatar_url']
+        )
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data)
