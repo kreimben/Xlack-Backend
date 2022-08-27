001408bcd4ea1f9820fd67a0985748574f87b262
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-27 18:29:01 +0900

    Add user's id field to easily find pk.

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -5,5 +5,5 @@ from user_profile.models import UserProfile
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'bio', 'created_at']
+    list_display = ['user', 'user_id', 'bio', 'created_at']
     search_fields = ['user_id', 'user_email']
