9cd333959c001f5d5a1f7d97c0daa3d189f24505
Aksidion Kreimben <aksidion@kreimben.com>
2022-08-27 18:10:20 +0900

    Add user field to implement new user serializer.

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -1,10 +1,17 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from user_profile.models import UserProfile
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+
 class UserProfileSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    user = UserSerializer(many=False, read_only=True)
 
     class Meta:
         model = UserProfile
