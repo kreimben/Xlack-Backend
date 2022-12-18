764c57f3ba877d910805b96ccc9350c5a7ffcc02
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-18 20:08:02 +0900

    Remove unnecessary library anymore.

--- xlack/settings.py
+++ xlack/settings.py
@@ -90,7 +90,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
 
