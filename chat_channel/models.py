a858bfd8aa66ad93692b9a04457f5727e2ff8b03
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-23 18:48:18 +0900

    Remove redundant comment.

--- chat_channel/models.py
+++ chat_channel/models.py
@@ -6,7 +6,6 @@ from xlack import settings
 
 # The reason I named `ChatChannel` is avoiding confusion with `django channels`.
 class ChatChannel(models.Model):
-    # Should we make chatted user field?
     name = models.CharField(max_length=50, unique=True)
     workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE, null=False, blank=False, related_name='chat_channel')
     description = models.TextField(null=True, blank=True)
