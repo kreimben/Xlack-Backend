7642d269a45690458739f2d2389e9aa67c41dd87
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-23 10:35:21 +0900

    Implement profile image feature.

--- custom_user/models.py
+++ custom_user/models.py
@@ -2,10 +2,16 @@ from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 from django.db import models
 
 
+def upload_img(instance, filename):
+    return f'media/profile_image/user_{instance.id}/{filename}'
+
+
 class CustomUser(AbstractUser):
     display_name = models.CharField(max_length=100, null=True, blank=True)
     title = models.CharField(max_length=100, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
+    profile_image = models.ImageField(upload_to=upload_img, null=True, blank=True)
+
     # Should have  to include profile image field.
 
     def __str__(self):
