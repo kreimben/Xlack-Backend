be62065c451be81af4341996c341ceb7e628f0b3
gyuri <rbfl0616@naver.com>
2022-12-23 16:10:59 +0900

    view

---
+++ file/views.py
@@ -0,0 +1,24 @@
+from rest_framework import viewsets, permissions
+from rest_framework.parsers import MultiPartParser
+
+from file.models import File
+from file.serializers import FileSerializer
+
+
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    parser_classes = [MultiPartParser]
+    http_method_names = ['get', 'post']
+
+    def perform_create(self, serializer):
+        serializer.save(uploaded_by=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        try:
+            file = File.objects.get(uploaded_by=self.request.user)
+            file.delete()
+        except File.DoesNotExist:
+            ...
+        return super().create(request, *args, **kwargs)
