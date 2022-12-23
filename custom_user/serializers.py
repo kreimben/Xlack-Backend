0157d0b43b1abada5db82b7e0154532768de7e0f
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-23 18:43:44 +0900

    Implement serializer which takes only name.

--- custom_user/serializers.py
+++ custom_user/serializers.py
@@ -4,7 +4,14 @@ from custom_user.models import CustomUser
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'display_name', 'title', 'phone_number', 'profile_image']
+
+
+class CustomUserNameSerializer(CustomUserSerializer):
+    email = serializers.CharField(read_only=True)
+    display_name = serializers.CharField(read_only=True)
+    title = serializers.CharField(read_only=True)
+    phone_number = serializers.CharField(read_only=True)
+    profile_image = serializers.CharField(read_only=True)
