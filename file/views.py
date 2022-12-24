ee65a67d50d58e7dd667fa9dfaa61cfdc9d900a9
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-24 21:35:19 +0900

    Implement `get_queryset` method to get a conditional queryset.

--- file/views.py
+++ file/views.py
@@ -10,7 +10,17 @@ class FileViewSet(viewsets.ModelViewSet):
     serializer_class = FileSerializer
     permission_classes = [permissions.IsAuthenticated]
     parser_classes = [MultiPartParser]
-    http_method_names = ['get', 'post']
+    http_method_names = ['get', 'post', 'delete']
+
+    def get_queryset(self):
+        """
+        If client request as `list` or method is `DELETE`, Get queryset about themselves,
+        If not (just retrieve for download file), Get not filtering queryset.
+        """
+        if self.kwargs.get('pk', None) and self.request.method == 'GET':
+            return self.queryset
+        else:
+            return self.queryset.filter(uploaded_by=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(uploaded_by=self.request.user)
