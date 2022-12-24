6107baa0266f2d398cab1b0c427745a5f82df783
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-24 21:36:40 +0900

    Add id field to identify objects.

--- file/admin.py
+++ file/admin.py
@@ -5,5 +5,5 @@ from file.models import File
 
 @admin.register(File)
 class FileAdmin(admin.ModelAdmin):
-    list_display = ['uploaded_by', 'file', 'created_at', 'updated_at']
+    list_display = ['id', 'uploaded_by', 'file', 'created_at', 'updated_at']
     search_fields = ['uploaded_by']
