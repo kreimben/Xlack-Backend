b8adc6446f6e9b08341beacfbaf1c17d250d2ad3
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-30 12:14:31 +0900

    Fix the way of auth and some variables.

--- notifications/consumers.py
+++ notifications/consumers.py
@@ -1,78 +1,78 @@
 import json
 
+from asgiref.sync import sync_to_async
 from channels.db import database_sync_to_async
-
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.exceptions import StopConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 from AuthHelper import AuthHelper, AccessTokenNotIncludedInHeader
-from rest_framework_simplejwt.exceptions import TokenError
-
+from custom_user.models import CustomUser
 from notifications import api
 
 
 class NotificationsConsumer(AsyncJsonWebsocketConsumer):
     @database_sync_to_async
-    def _create_notification_list(user_id):
-        api.get_notification_list(user_id)
+    def _create_notification_list(self, user_id):
+        return api.get_notification_list(user_id)
 
     @database_sync_to_async
-    def _read(user_id, sender, channel):
-        api.read_notification_list(receiver=user_id, sender=sender, channel=channel)
+    def _read(self, user_id, sender, channel):
+        return api.read_notification_list(receiver=user_id, sender=sender, channel=channel)
 
     @database_sync_to_async
     def _refresh(self, user_id):
         list_of_notification = self._create_notification_list(user_id)
-        return json.dumps(list_of_notification)
+        return list_of_notification
 
     async def connect(self):
         # check auth token
         try:
-            access_token = AuthHelper.find_access_token(self.scope)
-            if access_token is None:
-                raise TokenError
-        except TokenError:
-
-            self.send_json(content={"msg": "access token Not found."}, close=True)
-            self.close()
+            user = await sync_to_async(AuthHelper.find_user)(self.scope)
+            if user is None:
+                print(f'{user=}')
+                return
+        except AccessTokenNotIncludedInHeader:
+            print(f'access token was not in header.')
+            return
+        except CustomUser.DoesNotExist:
+            print(f'No such user.')
             return
 
-        _, user_id = AuthHelper.find_user(access_token)
-        list_of_notification = await self._create_notification_list(user_id)
+        list_of_notification = await self._create_notification_list(user.id)
 
-        self.accept()
+        await self.accept()
 
-        self.send_json(json.dumps(list_of_notification))
+        await self.send_json(list_of_notification)
 
     async def receive_json(self, content):
         """
         Find user model using access token
         and
         """
-        access_token = AuthHelper.find_access_token(
-            self.scope
-        )  # Don't check about access token.
-        _, user_id = AuthHelper.find_user(
-            access_token
-        )  # May return None if access token is expired.
-        if user_id is None:
-            self.send_json(
-                content={"msg": "access token is expired or invalid."}, close=True
-            )
+        try:
+            user = await sync_to_async(AuthHelper.find_user)(self.scope)
+        except AccessTokenNotIncludedInHeader:
+            print(f'access token was not in header.')
+            await self.send_json(content={'msg': 'access token was not in header.'}, close=True)
+            return
+        except CustomUser.DoesNotExist:
+            print(f'No such user.')
+            await self.send_json(content={'msg': 'No such user.'}, close=True)
+            return
 
         if content.refresh:
-            await self._refresh(user_id)
+            await self._refresh(user.id)
 
         else:
             if content.sender and content.channel == None:
-                self.send_json(content={"msg": "sender and channel are invalid."})
+                await self.send_json(content={"msg": "sender and channel are invalid."})
             else:
                 await self._read(
-                    receiver=user_id, sender=content.sender, channel=content.channel
+                    receiver=user.id, sender=content.sender, channel=content.channel
                 )
-                new_notifications = await self._refresh(user_id)
+                new_notifications = await self._refresh(user.id)
 
-                self.send_json(content=new_notifications)
+                await self.send_json(content=new_notifications)
 
     def disconnect(self, code):
         raise StopConsumer()
