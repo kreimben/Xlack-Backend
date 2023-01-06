95be8fed39295b9324e42fd8b8d136f1a3652df8
Aksidion Kreimben <aksidion@kreimben.com>
2023-01-06 21:54:53 +0900

    Implement feature that display what I bookmarked on chat and `get_queryset` in order to fix standard to find chat channel (chat_channel id -> chat_channel hashed_value)

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions, generics, status
+from rest_framework import generics, status
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.request import Request
@@ -13,15 +13,34 @@ class ChatView(generics.ListAPIView):
     count는 전체 레코드의 수
     next, previous는 이전, 이후의 url
     """
-    permission_classes = [permissions.IsAuthenticated]
     serializer_class = ChatSerializer
     pagination_class = LimitOffsetPagination
-    lookup_field = 'channel_id'
-    queryset = Chat.objects.order_by('-id').all()
+    queryset = Chat.objects.all()
 
     def get_queryset(self):
-        cid = self.kwargs['channel_id']
-        return self.queryset.filter(channel_id=cid)
+        chv = self.kwargs.get('channel__hashed_value', None)
+        return self.queryset.filter(channel__hashed_value__exact=chv).order_by('-id')
+
+    def get(self, request: Request, *args, **kwargs):
+        """
+        url query에 `limit`, `offset`을 넣지 않으면 전체 값으로 일반 배열 형태로 결과가 나오고,
+        넣었다면 아래 문서와 같이 results에 배열로 값이 들어갑니다.
+        `has_bookmarked` field는 오직 "내가 북마크 했는지"만 표시됨으로, true 혹은 false값이 나옵니다.
+        """
+        q = self.get_queryset()
+        page = self.paginate_queryset(q)
+        if page is not None:
+            s = self.get_serializer(page, many=True)
+            return self.get_paginated_response(s.data)
+        else:
+            s = self.get_serializer(q, many=True)
+            for data in s.data[:]:
+                try:
+                    b = ChatBookmark.objects.get(issuer=self.request.user, chat_id=data['id'])
+                except ChatBookmark.DoesNotExist:
+                    b = None
+                data['has_bookmarked'] = True if b is not None else False
+            return Response(s.data)
 
 
 class ChatBookmarkCreateView(generics.CreateAPIView):
@@ -46,6 +65,5 @@ class ChatBookmarkDeleteView(generics.DestroyAPIView):
     def delete(self, request: Request, *args, **kwargs):
         chat_id = kwargs.get('chat_id', None)
         chat_bookmark = get_object_or_404(ChatBookmark, issuer=request.user, chat_id=chat_id)
-        print(f'{chat_bookmark=}')
         chat_bookmark.delete()
         return Response(status=status.HTTP_302_FOUND)
