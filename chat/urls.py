ff69e83c88ebfdfb76958b69ac65e41a351134a8
Aksidion Kreimben <aksidion@kreimben.com>
2023-01-06 21:49:33 +0900

    Fix path priority.

--- chat/urls.py
+++ chat/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from chat import views
 
 urlpatterns = [
-    path('<int:channel_id>/', views.ChatView.as_view()),
     path('bookmark/', views.ChatBookmarkCreateView.as_view()),
-    path('bookmark/<int:chat_id>/', views.ChatBookmarkDeleteView.as_view())
+    path('bookmark/<int:chat_id>/', views.ChatBookmarkDeleteView.as_view()),
+    path('<str:channel__hashed_value>/', views.ChatView.as_view()),
 ]
