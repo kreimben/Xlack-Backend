5972d0cb48219551353ba2a41afab756a17f3aad
Aksidion Kreimben <aksidion@kreimben.com>
2023-01-08 19:54:54 +0900

    Add fields for convenient.

--- chat_channel/admin.py
+++ chat_channel/admin.py
@@ -5,5 +5,6 @@ from chat_channel.models import ChatChannel
 
 @admin.register(ChatChannel)
 class ChatChannelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'hashed_value', 'description', 'created_at', 'updated_at']
+    list_display = ['id', 'name', 'hashed_value', 'description', 'is_dm', 'workspace', 'created_at', 'updated_at']
     search_fields = ['name']
+    list_filter = ['is_dm', 'workspace']
