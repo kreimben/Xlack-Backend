42a8d4fc618307a55d1299d1e5c93cd7bc49a403
Aksidion Kreimben <aksidion@kreimben.com>
2023-01-08 19:55:11 +0900

    Implement serializer for fit.

---
+++ direct_message/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+class DMCreateSerializer(serializers.Serializer):
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
+
+    target_user_id = serializers.IntegerField()
