fa09df2a25aff5e8f7f761ce60d99f9a2bc6d067
ds1sqe <ds1sqe@mensakorea.org>
2023-01-22 07:33:12 +0900

    added endpoint

--- chat/urls.py
+++ chat/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 from chat import views
 
+from chat_reaction.views import ChatReactionView
+
 urlpatterns = [
     path('bookmark/', views.ChatBookmarkCreateView.as_view()),
     path('bookmark/<int:chat_id>/', views.ChatBookmarkDeleteView.as_view()),
+    path('reaction/', ChatReactionView.as_view()),
     path('<str:channel__hashed_value>/', views.ChatView.as_view()),
-    path('reaction/', views.ChatReactionCreateView.as_view()),
-    path('reaction/remove/', views.ChatReactionRemoveView.as_view())
 ]
