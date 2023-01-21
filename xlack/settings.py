0cb8c6a71635d46de87127b6b2c07c8c8bcbc486
ds1sqe <ds1sqe@mensakorea.org>
2023-01-22 07:39:19 +0900

    trusted origin had modifed

--- xlack/settings.py
+++ xlack/settings.py
@@ -277,10 +277,12 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
-DATA_UPLOAD_MAX_MEMORY_SIZE = 1024 * 10  # https://stackoverflow.com/a/54539084/10684515
+# https://stackoverflow.com/a/54539084/10684515
+DATA_UPLOAD_MAX_MEMORY_SIZE = 1024 * 10
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
+    'http://localhost:8000',
     'https://xlack.kreimben.com'
 )
 
