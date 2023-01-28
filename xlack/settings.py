d3c0521acf08493911eb8b15d64b3cab4a24ac15
Aksidion Kreimben <aksidion@kreimben.com>
2023-01-28 13:52:51 +0900

    Add new dependency for profiling.

--- xlack/settings.py
+++ xlack/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'dj_rest_auth.registration',
     'corsheaders',
     'daphne',
+    'silk',
 
     # Django Native App.
     'django.contrib.admin',
@@ -88,6 +89,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'silk.middleware.SilkyMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -285,3 +288,5 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 CORS_ALLOW_CREDENTIALS = True
+
+SILKY_PYTHON_PROFILER = True
