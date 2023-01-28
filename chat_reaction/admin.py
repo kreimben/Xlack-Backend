9c1795c372fcd999a795e96b271eb5220b1231ee
Aksidion Kreimben <aksidion@kreimben.com>
2023-01-29 00:54:41 +0900

    Implement the way to display many to many fields on django admin.

--- chat_reaction/admin.py
+++ chat_reaction/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 
 from chat_reaction.models import ChatReaction
-
 from chat_reaction.serializers import Util
 
 
 @admin.register(ChatReaction)
 class ChatReactionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'chat', 'get_icon', 'get_reactors']
 
     def icon_repr(self, obj):
         return Util.to_repr(obj.icon)
 
-    list_display = ['id', 'chat', 'icon_repr']
+    def get_reactors(self, obj: ChatReaction):
+        return ', '.join([str(user) for user in obj.reactors.all()])
