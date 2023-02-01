4d70f1a06518d998ebd6538461aec47bd14684bd
Aksidion Kreimben <aksidion@kreimben.com>
2023-02-01 14:13:47 +0900

    Fix redis host.

--- xlack/settings.py
+++ xlack/settings.py
@@ -148,7 +148,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(os.getenv('DB_HOST'), os.getenv('REDIS_PORT'))],
+            "hosts": [(os.getenv('REDIS_HOST'), os.getenv('REDIS_PORT'))],
         },
     },
 }
