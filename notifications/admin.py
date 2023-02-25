af3e50c811d99aebfb2fc2c00b4d8c248419896b
Aksidion Kreimben <aksidion@kreimben.com>
2023-02-25 21:59:07 +0900

    Add actions about `had_read` field.

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
 from notifications.models import Notification
 
@@ -8,3 +8,19 @@ class NotificationAdmin(admin.ModelAdmin):
     list_display = ["id", "sender", "receiver", 'chat', "channel", "had_read"]
     search_fields = ["sender", "receiver", "channel"]
     list_filter = ['sender', 'receiver', 'channel', 'had_read']
+    actions = ['make_read', 'make_unread']
+    list_select_related = ['sender', 'receiver', 'channel']
+
+    @admin.action(description='선택한 알림들을 읽음 처리 합니다.')
+    def make_read(self, request, queryset):
+        queryset.had_read = True
+        self.message_user(request,
+                          f'{queryset}이 읽음 처리 됐습니다.' if len(queryset) == 1 else f'{len(queryset)}개의 알림이 읽음 처리 됐습니다.',
+                          messages.SUCCESS)
+
+    @admin.action(description='선택한 알림들을 안읽음 처리 합니다.')
+    def make_unread(self, request, queryset):
+        queryset.had_read = False
+        self.message_user(request,
+                          f'{queryset}이 안읽음 처리 됐습니다.' if len(queryset) == 1 else f'{len(queryset)}개의 알림이 안읽음 처리 됐습니다.',
+                          messages.SUCCESS)
