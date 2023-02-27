d542243818ba549826288615bba232f7c9c5f401
Aksidion Kreimben <aksidion@kreimben.com>
2023-02-27 15:57:11 +0900

    Add view to url.

--- workspace/urls.py
+++ workspace/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from workspace.views import WorkspaceView
+from workspace.views import WorkspaceView, WorkspaceBookmarkedChatView
 
 urlpatterns = [
-    path('', WorkspaceView.as_view(), name='workspace')
+    path('', WorkspaceView.as_view(), name='workspace'),
+    path('bookmarked_chat/<str:workspace_hashed_value>', WorkspaceBookmarkedChatView.as_view(), name='workspace_bookmarked_chats'),
 ]
