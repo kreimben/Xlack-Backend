4386e60d9626d54a71ea3cd46099f9289891ba0d
ds1sqe <ds1sqe@mensakorea.org>
2023-03-02 17:08:31 +0900

    wip on api

--- chat_counter/api.py
+++ chat_counter/api.py
@@ -6,24 +6,23 @@ from .serializers import ChatCounterSerializer
 
 
 class CounterApi:
-    user = Null
-    channel = Null
-    most_recent_chat = Null
-    queryset = Counter.objects.all()
+    counter = Counter.objects.all()
+    readinfo = ReadInfo.objects.all()
 
-    def __get_queryset(self):
+    def __get_counter(self, **kwargs):
         chv = self.kwargs.get("channel__hashed_value", None)
-        user = self.kwargs.get("user", None)
         if chv is None:
-            raise ValueError("CounterApi>>__get_queryset:ERROR, channel is None", chv)
+            raise ValueError("CounterApi>>__get_counter:ERROR, channel is None", chv)
         else:
             return (
-                self.queryset.select_related("channel")
+                self.counter.select_related("channel", "readinfo")
                 .filter(channel__hashed_value__exact=chv)
                 .prefetch_related(
                     Prefetch(
                         "readinfo",
-                        queryset=ReadInfo.objects.filter(
+                        queryset=ReadInfo.objects.select_related(
+                            "user", "most_recent_chat"
+                        ).filter(
                             most_recent_chat__channel__hashed_value__exact=chv,
                             counter__channel__hashed__value__exact=chv,
                         ),
@@ -31,17 +30,35 @@ class CounterApi:
                 )
             )
 
-    def get(self, channel):
+    def __get_readinfo(self, **kwargs):
+        user = self.kwargs.get("user", None)
+        most_recent_chat_before = self.kwargs.get("most_recent_chat_before", None)
+        if user is None:
+            raise ValueError("CounterApi>>__get_readinfo:ERROR, user is None", user)
+        elif most_recent_chat is None:
+            raise ValueError(
+                "CounterApi>>__get_readinfo:ERROR, most_recent_chat is None",
+                most_recent_chat,
+            )
+        else:
+            return (
+                self.readinfo.select_related("counter", "most_recent_chat")
+                .filter(channel__hashed_value__exact=chv)
+                .get_or_create(user=kwargs.get(user))
+                )
+            )
+
+    def get(self, **kwargs):
         """
         Get or create counter which depends on chat channel,
         and return instance of it
         """
-        Counter.objects.get_or_create(channel=channel)
+        qs = self.__get_counter(**kwargs)
+        return qs
 
-        pass
-
-    def update(self, channel, user, chat):
+    def update(self, **kwargs):
         """
         Update counter's read info data
         """
-        pass
+        qs = self.__get_readinfo(**kwargs)
+        return qs
