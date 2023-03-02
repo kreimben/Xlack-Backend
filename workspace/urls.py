c9f212f87379a865a9b0f2fe77400ea4ef4e380a
Aksidion Kreimben <aksidion@kreimben.com>
2023-03-02 17:25:16 +0900

    Add missing slash.

--- workspace/urls.py
+++ workspace/urls.py
@@ -4,5 +4,5 @@ from workspace.views import WorkspaceView, WorkspaceBookmarkedChatView
 
 urlpatterns = [
     path('', WorkspaceView.as_view(), name='workspace'),
-    path('bookmarked_chat/<str:workspace_hashed_value>', WorkspaceBookmarkedChatView.as_view(), name='workspace_bookmarked_chats'),
+    path('bookmarked_chat/<str:workspace_hashed_value>/', WorkspaceBookmarkedChatView.as_view(), name='workspace_bookmarked_chats'),
 ]
