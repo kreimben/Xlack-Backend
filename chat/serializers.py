b6af3b7180e256713796408779083e1e1daa1d80
Aksidion Kreimben <aksidion@kreimben.com>
2023-05-25 05:20:19 +0900

    Fix date format.

--- chat/serializers.py
+++ chat/serializers.py
@@ -18,7 +18,7 @@ class ChatSerializer(serializers.Serializer):
     converted_created_at = serializers.SerializerMethodField(read_only=True)
 
     def get_converted_created_at(self, obj):
-        return obj.created_at.strftime("%d %b %Y %H:%M:%S")
+        return obj.created_at.strftime('%a %b %d %Y %H:%M:%S')
 
     def create(self, validated_data):
         data = {}
